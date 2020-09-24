c7f78b28866b5fac62f281c462dfa74074131f05
aure-nogueras <anogueras@correo.ugr.es>
2020-09-24 23:40:03 +0200

    Añadido logging a la aplicación

--- DatosDemograficos/webapp.py
+++ DatosDemograficos/webapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, current_app
 import requests
 import json
 from Datos.settings import *
@@ -7,6 +7,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+	current_app.logger.info('Mostrando la portada de la aplicación')
 	#Añadir formulario con los filtros de búsqueda
 	return render_template('index.html')
 	#return redirect(url_for('buscar', name="filtro"))
@@ -17,21 +18,29 @@ def map(name):
     jsonInfo = json.loads(info.text)
     
     if(jsonInfo == TOTAL_POB_MAPA):
-	    return render_template('total_pob_mapa.html')
+        current_app.logger.info('Mostrando mapa del total de la población')
+        return render_template('total_pob_mapa.html')
     elif (jsonInfo == HOMBRE_POB_MAPA):
-	    return render_template('hombre_pob_mapa.html')
+    	current_app.logger.info('Mostrando mapa de la población de hombres')
+    	return render_template('hombre_pob_mapa.html')
     elif (jsonInfo == MUJER_POB_MAPA):
-	    return render_template('mujer_pob_mapa.html')
+    	current_app.logger.info('Mostrando mapa de la población de mujeres')
+    	return render_template('mujer_pob_mapa.html')
     elif (jsonInfo == TOTAL_POB_BAR):
-	    return render_template('total_pob_bar.html')
+    	current_app.logger.info('Mostrando un gráfico de barras del total de la población')
+    	return render_template('total_pob_bar.html')
     elif (jsonInfo == HOMBRE_POB_BAR):
-	    return render_template('hombre_pob_bar.html')
+    	current_app.logger.info('Mostrando un gráfico de barras de la población de hombres')
+    	return render_template('hombre_pob_bar.html')
     elif (jsonInfo == MUJER_POB_BAR):
-	    return render_template('mujer_pob_bar.html')
+    	current_app.logger.info('Mostrando un gráfico de barras de la población de mujeres')
+    	return render_template('mujer_pob_bar.html')
     elif (jsonInfo == AMBOS_POB_BAR):
-	    return render_template('ambos_pob_bar.html')
+    	current_app.logger.info('Mostrando un gráfico de barras de hombres/mujeres')
+    	return render_template('ambos_pob_bar.html')
     else:
-        return render_template('non_valid.html')
+    	current_app.logger.info('Búsqueda no válida')
+    	return render_template('non_valid.html')
     
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
